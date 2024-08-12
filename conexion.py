a3c5236537279c4704e97e01d491795e33afe97d
Leonardo Coutiño Tamayo <87379933+Leoct97@users.noreply.github.com>
2024-08-12 14:52:02 -0600

    Update conexion.py

--- conexion.py
+++ conexion.py
@@ -1,11 +1,11 @@
 import sqlite3
 
 
-# con = sqlite3.connect("gs.db")
-# cur = con.cursor()
-# cur.execute("CREATE TABLE productos(id,marca,producto,fecha_alta)")
+con = sqlite3.connect("gs.db")
+cur = con.cursor()
+cur.execute("CREATE TABLE productos(id,marca,producto,fecha_alta)")
 
 
-# con = sqlite3.connect("gs.db")
-# cur = con.cursor()
-# cur.execute("CREATE TABLE inventario(id,marca,producto,inventario,fecha_alta,fecha_baja)")
+con = sqlite3.connect("gs.db")
+cur = con.cursor()
+cur.execute("CREATE TABLE inventario(id,marca,producto,inventario,fecha_alta,fecha_baja)")
